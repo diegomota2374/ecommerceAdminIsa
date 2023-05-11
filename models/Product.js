32aeec21ecb329b43b31502c5087b6dd03ae40e0
diegomota2374 <diegomota2374@gmail.com>
2023-05-11 17:48:37 -0300

    foi configurado o component de upload de imagens

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,7 @@ const ProductSchema = new Schema({
   title: String,
   description: String,
   price: { type: Number, required: true },
+  images: [{ type: String }],
 });
 
 export const Product = models.Product || model("Product", ProductSchema);
