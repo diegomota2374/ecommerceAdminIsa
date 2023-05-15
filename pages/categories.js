41ef626c5d8e50dd3889530a6faf6bd11d41b15d
diegomota2374 <diegomota2374@gmail.com>
2023-05-15 10:56:04 -0300

    foi configurado a função delete com popup de forma simplificada das categorias

---
+++ pages/categories.js
@@ -0,0 +1,139 @@
+import Layout from "@/components/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { withSwal } from "react-sweetalert2";
+
+function Categories({ swal }) {
+  const [editedCategory, setEditedCategory] = useState(null);
+  const [name, setName] = useState("");
+  const [parentCategory, setParentCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  function fetchCategories() {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }
+
+  async function saveCategory(ev) {
+    ev.preventDefault();
+    const data = { name, parentCategory };
+    if (editedCategory) {
+      data._id = editedCategory._id;
+      await axios.put("/api/categories", data);
+      setEditedCategory(null);
+    } else {
+      await axios.post("/api/categories", data);
+    }
+    setName("");
+    fetchCategories();
+  }
+
+  function editCategory(category) {
+    setEditedCategory(category);
+    setName(category.name);
+    setParentCategory(category.parent?._id);
+  }
+
+  function deleteCategory(category) {
+    swal
+      .fire({
+        title: "Are you sure?",
+        text: `Do you want to delete ${category.name}?`,
+        showCancelButton: true,
+        cancelButtonText: "Cancel",
+        confirmButtonText: "Yes, Delete!",
+        confirmButtonColor: "#d55",
+
+        didOpen: () => {
+          // run when swal is opened...
+        },
+        didClose: () => {
+          // run when swal is closed...
+        },
+      })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          const { _id } = category;
+          await axios.delete("/api/categories?_id=" + _id);
+          fetchCategories();
+        }
+      })
+      .catch((error) => {
+        // when promise rejected...
+      });
+  }
+
+  return (
+    <Layout>
+      <h1>Categories</h1>
+      <label>
+        {editedCategory
+          ? `Edit category ${editedCategory.name}`
+          : "Create new category"}
+      </label>
+      <form onSubmit={saveCategory} className={"flex gap-1"}>
+        <input
+          className={"mb-0"}
+          type="text"
+          placeholder={"Category name"}
+          onChange={(ev) => setName(ev.target.value)}
+          value={name}
+        />
+        <select
+          className={"mb-0"}
+          onChange={(ev) => setParentCategory(ev.target.value)}
+          value={parentCategory}
+        >
+          <option value={""}>No parent category</option>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <option key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+        </select>
+        <button type={"submit"} className={"btn-primary py-1"}>
+          Save
+        </button>
+      </form>
+      <table className={"basic mt-4"}>
+        <thead>
+          <tr>
+            <td>Category name</td>
+            <td>Parent category</td>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((category) => (
+              <tr key={category._id}>
+                <td>{category.name}</td>
+                <td>{category?.parent?.name}</td>
+                <td>
+                  <button
+                    onClick={() => editCategory(category)}
+                    className={"btn-primary mr-1"}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => deleteCategory(category)}
+                    className={"btn-primary"}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
+
+export default withSwal(({ swal }, ref) => <Categories swal={swal} />);
